55930bc2788a18ae971c18a777a6896714652254
Michael <eramehmichael2000@gmail.com>
2025-12-18 17:14:25 +0100

    update

--- connection.php
+++ connection.php
@@ -2,10 +2,10 @@
 // Database connection (Change credentials accordingly)
         $servername = "localhost";
         //$username = "dualstre_Apex Finance Credit";
-        $username = "root";
-        $password = "";
+        $username = "dginland_apexfinance";
+        $password = "dginland_apexfinance";
         //$dbname = "dualstre_Apex Finance Credit";
-        $dbname = "bremt";
+        $dbname = "rtxwb3TJqT4YkN9JLjav";
 
         $conn = new mysqli($servername, $username, $password, $dbname);
         // if($conn){
