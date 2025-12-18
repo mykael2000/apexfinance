86acb8942f272abf400a5457ef16d3f12fd2154d
Michael <eramehmichael2000@gmail.com>
2025-12-18 17:18:15 +0100

    update

--- connection.php
+++ connection.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Database connection (Change credentials accordingly)
         $servername = "localhost";
         //$username = "dualstre_Apex Finance Credit";
